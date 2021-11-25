9717e56a58fc661ffeaa339a641f4bade7bbbca0
Mihai Cherechesu <mihai.cherechesu@gmail.com>
2021-11-24 22:06:59 -0800

    Added handlers for input commands

--- tema1/final/api_client.cpp
+++ tema1/final/api_client.cpp
@@ -13,193 +13,18 @@ map<string, op_type> op_stoi = {
 	{ "get_stat_all", 	(op_type) GET_STAT_ALL }
 };
 
-void
-handle_login(string username)
-{
-	cout << "Executing login " << username << "...\n";
-	return;
-}
+unsigned long session_key;
+char *username;
 
 void
-handle_logout()
+handle_login(string uname, CLIENT *clnt)
 {
-	printf("Executing logout...\n");
-	return;	
-}
-
-void
-handle_load()
-{
-	printf("Executing load...\n");
-	return;
-}
-
-void
-handle_store()
-{
-	printf("Executing store...\n");
-	return;	
-}
-
-void
-handle_add(int sensor_id, int data_len, float *data)
-{
-	printf("Executing add %d %d [vals]...\n", sensor_id, data_len);
-	return;	
-}
-
-void
-handle_del(int sensor_id)
-{
-	printf("Executing del %d...\n", sensor_id);
-	return;	
-}
-
-void
-handle_update(int sensor_id, int data_len, float *data)
-{
-	printf("Executing update %d %d [vals]...\n", sensor_id, data_len);
-	return;	
-}
-
-void
-handle_read(int sensor_id)
-{
-	printf("Executing read %d...\n", sensor_id);
-	return;
-}
-
-void
-handle_get_stat(int sensor_id)
-{
-	printf("Executing get_stat %d...\n", sensor_id);
-	return;
-}
-
-void
-handle_get_stat_all()
-{
-	printf("Executing get_stat_all...\n");
-	return;
-}
-
-void
-execute_input(string input_file)
-{
-	ifstream in(input_file);
-	string line, token;
-
-	if (!in) {
-		cout << "[FATAL]: Unable to open input file " << input_file << "!\n";
-		exit(1);
-	}
-
-	while (getline(in, line)) {
-		stringstream ss(line);
-		vector<string> tokens;
-
-		while (getline(ss, token, ' ')) {
-			tokens.push_back(token);
-		}
-
-		switch(op_stoi[tokens[TKN_CMD]]) {
-			case LOGIN: {
-				string username = tokens[TKN_USERNAME];
-				handle_login(username);
-				break;
-			}
-
-			case LOGOUT: {	
-				handle_logout();
-				break;
-			}
-			
-			case LOAD: {
-				handle_load();
-				break;
-			}
-
-			case STORE: {
-				handle_store();
-				break;
-			}
-
-			case ADD: {
-				int sensor_id = stoi(tokens[TKN_SID]);
-				int data_len = stoi(tokens[TKN_SDATA_LEN]);
-				float *data = (float *) calloc(data_len, sizeof(float));
-
-				for (int i = 0; i < data_len; i++) {
-					data[i] = stof(tokens[TKN_SDATA_VAL + i]);
-				}
-
-				handle_add(sensor_id, data_len, data);
-				break;
-			}
-
-			case DEL: {
-				int sensor_id = stoi(tokens[TKN_SID]);
-				handle_del(sensor_id);
-				break;
-			}
-
-			case UPDATE: {
-				int sensor_id = stoi(tokens[TKN_SID]);
-				int data_len = stoi(tokens[TKN_SDATA_LEN]);
-				float *data = (float *) calloc(data_len, sizeof(float));
-
-				for (int i = 0; i < data_len; i++) {
-					data[i] = stof(tokens[TKN_SDATA_VAL + i]);
-				}
-
-				handle_update(sensor_id, data_len, data);
-				break;
-			}
-
-			case READ: {
-				int sensor_id = stoi(tokens[TKN_SID]);
-				handle_read(sensor_id);
-				break;
-			}
-
-			case GET_STAT: {
-				int sensor_id = stoi(tokens[TKN_SID]);
-				handle_get_stat(sensor_id);
-				break;
-			}
-
-			case GET_STAT_ALL: {
-				handle_get_stat_all();
-				break;
-			}
-			default: 
-				cout << "[ERROR]: Cannot execute command " << tokens[TKN_CMD] << "!\n";
-    	}
-	}
-	
-	in.close();
-}
-
-void
-api_prog_1(char *host, char *input_file)
-{
-	CLIENT *clnt;
-	unsigned long session_key = 0UL;
-	char *username;
-
-	clnt = clnt_create (host, api_prog, api_vers, "udp");
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		exit (1);
-	}
-
-	execute_input(string(input_file));
-	exit(0);
+	cout << "Executing login " << uname << "...\n";
 
 	rpc_msg_ *result_1;
 	rpc_msg_  login_1_arg = {
 		(err_type) SUCC,
-		{ { (msg_type_) REQ, { 0UL, { (op_type) LOGIN, { .r0 = { (char*) "client_1" } } } } } }
+		{ { (msg_type_) REQ, { 0UL, { (op_type) LOGIN, { .r0 = { &uname[0] } } } } } }
 	};
 
 	result_1 = login_1(&login_1_arg, clnt);
@@ -218,28 +43,16 @@ api_prog_1(char *host, char *input_file)
 		session_key = result_1->rpc_msg__u.success
 			.rpc_success_u.repl.body
 			.rpc_reply_body_u.r0.new_session_key;
+
+		username = (char *) calloc(uname.size(), sizeof(char));
+		strcpy(username, &uname[0]);
 	}
+}
 
-	rpc_msg_  *result_1_1;
-	rpc_msg_  login_2_arg = {
-		(err_type) SUCC,
-		{ { (msg_type_) REQ, { 0UL, { (op_type) LOGIN, { .r0 = { (char*) "client_1" } } } } } }
-	};
-
-	result_1_1 = login_1(&login_2_arg, clnt);
-	if (result_1_1 == (rpc_msg_ *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-
-	if (result_1_1->err)
-		printf("failure_message: %s, failure_code: %d\n\n",
-			result_1_1->rpc_msg__u.failure.failure_message,
-			result_1_1->rpc_msg__u.failure.failure_code);
-	else
-		printf("success_message: %s\n\n", 
-			result_1_1->rpc_msg__u.success.rpc_success_u.repl.success_message);
-
-	///////////////////////////////////////////////////////////////////////////
+void
+handle_logout(CLIENT *clnt)
+{
+	printf("Executing logout...\n");
 
 	rpc_msg_  *result_2;
 	rpc_msg_  logout_1_arg = {
@@ -247,56 +60,78 @@ api_prog_1(char *host, char *input_file)
 		{ { (msg_type_) REQ, { session_key, { (op_type) LOGOUT, { .r1 = { 0x1 } } } } } }
 	};
 
-	// result_2 = logout_1(&logout_1_arg, clnt);
-	// if (result_2 == (rpc_msg_ *) NULL) {
-	// 	clnt_perror (clnt, "call failed");
-	// }
+	result_2 = logout_1(&logout_1_arg, clnt);
+	if (result_2 == (rpc_msg_ *) NULL) {
+		clnt_perror (clnt, "call failed");
+	}
 
-	// if (result_2->err)
-	// 	printf("failure_message: %s, failure_code: %d\n",
-	// 		result_2->rpc_msg__u.failure.failure_message,
-	// 		result_2->rpc_msg__u.failure.failure_code);
-	// else
-	// 	printf("success_message: %s\n", 
-	// 		result_2->rpc_msg__u.success.rpc_success_u.repl.success_message);
+	if (result_2->err)
+		printf("failure_message: %s, failure_code: %d\n",
+			result_2->rpc_msg__u.failure.failure_message,
+			result_2->rpc_msg__u.failure.failure_code);
+	else
+		printf("success_message: %s\n", 
+			result_2->rpc_msg__u.success.rpc_success_u.repl.success_message);
+	return;	
+}
 
-	///////////////////////////////////////////////////////////////////////////
+bool file_is_empty(ifstream& file)
+{
+    return file.peek() == ifstream::traits_type::eof();
+}
 
-	// string db_file = "client_db_" + string(username) + ".rpcdb";
-	// ifstream in(db_file, ios::in | ios::binary);
+void
+handle_load(CLIENT *clnt)
+{
+	printf("Executing load...\n");
 
-	// unsigned int num_sensors;
-	// unsigned int num_values;
-	// int sensor_id;
-	// float *values;
-	// sensor_data *sdl;
+	string db_file = "client_db_" + string(username) + ".rpcdb";
+	ifstream in(db_file, ios::in | ios::binary);
 
-	// in.read((char *) &num_sensors, sizeof num_sensors);
-	// sdl = (sensor_data *) calloc(num_sensors, sizeof(sensor_data));
+	if (!in) {
+		cout << "[FATAL]: Could not open file " << db_file << "!\n";
+		exit(1);
+	}
 
-	// for (int i = 0; i < num_sensors; i++) {
-	// 	in.read((char *) &num_values, sizeof num_values);
-	// 	in.read((char *) &sensor_id, sizeof sensor_id);
+	if (file_is_empty(in)) {
+		cout << "File " << db_file << " has been opened and initialized!\n";
+		in.close();
 
-	// 	values = (float *) calloc(num_values, sizeof(float));
-	// 	in.read((char *) &values, sizeof values);
+		return;
+	}
 
-	// 	printf("Sensor %d with %d values: ", sensor_id, num_values);
-	// 	for (int j = 0; j < num_values; j++) {
-	// 		printf("%f ", values[j]);
-	// 	}
-	// 	printf("\n");
+	cout << "Opening file: " << db_file << "\n";
 
-	// 	sdl[i] = { sensor_id, { num_values, values } };
-	// }
+	unsigned int num_sensors;
+	unsigned int num_values;
+	int sensor_id;
+	float *values;
+	sensor_data *sdl;
 
-	unsigned int num_sensors = 2;
-	float float_arr_1[] = {1.1, 1.1, 1.1};
-	float float_arr_2[] = {1.2, 1.2, 1.2};
+	in.read((char *) &num_sensors, sizeof num_sensors);
+	sdl = (sensor_data *) calloc(num_sensors, sizeof(sensor_data));
 
-	sensor_data s1 = { 1, { 3, float_arr_1 } };
-	sensor_data s2 = { 2, { 3, float_arr_2 } };
-	sensor_data sdl[] = {s1, s2};
+	printf("----> Number of sensors: %d\n", num_sensors);
+
+	for (int i = 0; i < num_sensors; i++) {
+		in.read((char *) &num_values, sizeof num_values);
+		printf("Here1!\n");
+		in.read((char *) &sensor_id, sizeof sensor_id);
+		printf("Here2!\n");
+		values = (float *) calloc(num_values, sizeof(float));
+		in.read((char *) &values, num_values * sizeof(float));
+
+		printf("\tSensor %d with %d values: (", sensor_id, num_values);
+
+		for (int j = 0; j < num_values; j++) {
+			printf("%f%s", values[j], j == num_values - 1 ? ")\n" : ", ");
+		}
+
+		sdl[i] = { sensor_id, { num_values, values } };
+	}
+
+	printf("\n");
+	in.close();
 
 	rpc_msg_  *result_3;
 	rpc_msg_  load_1_arg = {
@@ -316,96 +151,12 @@ api_prog_1(char *host, char *input_file)
 	else
 		printf("success_message: %s\n\n", 
 			result_3->rpc_msg__u.success.rpc_success_u.repl.success_message);
+}
 
-	///////////////////////////////////////////////////////////////////////////
-
-	float float_arr_3[] = {1.25, 7.69};
-	sensor_data s3 = { 1, { 2, float_arr_3 } };
-
-	rpc_msg_  *result_5;
-	rpc_msg_  add_1_arg = {
-		(err_type) SUCC,
-		{ { (msg_type_) REQ, { session_key, { (op_type) ADD, { .r4 = { s3 } } } } } }
-	};
-
-	result_5 = add_1(&add_1_arg, clnt);
-	if (result_5 == (rpc_msg_ *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-
-	if (result_5->err)
-		printf("failure_message: %s, failure_code: %d\n\n",
-			result_5->rpc_msg__u.failure.failure_message,
-			result_5->rpc_msg__u.failure.failure_code);
-	else
-		printf("success_message: %s\n\n", 
-			result_5->rpc_msg__u.success.rpc_success_u.repl.success_message);
-
-	sensor_data s4 = { 1111, { 2, float_arr_3 } };
-
-	add_1_arg = {
-		(err_type) SUCC,
-		{ { (msg_type_) REQ, { session_key, { (op_type) ADD, { .r4 = { s4 } } } } } }
-	};
-
-	result_5 = add_1(&add_1_arg, clnt);
-	if (result_5 == (rpc_msg_ *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-
-	if (result_5->err)
-		printf("failure_message: %s, failure_code: %d\n\n",
-			result_5->rpc_msg__u.failure.failure_message,
-			result_5->rpc_msg__u.failure.failure_code);
-	else
-		printf("success_message: %s\n\n", 
-			result_5->rpc_msg__u.success.rpc_success_u.repl.success_message);
-
-	///////////////////////////////////////////////////////////////////////////
-	float float_arr_4[] = {0.1, 1.5, 3.555, 9.13123};
-	sensor_data s5 = { 1, { 4, float_arr_4 } };
-
-	rpc_msg_ *result_7;
-	rpc_msg_  update_1_arg = {
-		(err_type) SUCC,
-		{ { (msg_type_) REQ, { session_key, { (op_type) UPDATE, { .r6 = { s5 } } } } } }
-	};
-
-	result_7 = update_1(&update_1_arg, clnt);
-	if (result_7 == (rpc_msg_ *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-
-	if (result_7->err)
-		printf("failure_message: %s, failure_code: %d\n\n",
-			result_7->rpc_msg__u.failure.failure_message,
-			result_7->rpc_msg__u.failure.failure_code);
-	else
-		printf("success_message: %s\n\n", 
-			result_7->rpc_msg__u.success.rpc_success_u.repl.success_message);
-
-	///////////////////////////////////////////////////////////////////////////
-
-	rpc_msg_  *result_6;
-	rpc_msg_  del_1_arg = {
-		(err_type) SUCC,
-		{ { (msg_type_) REQ, { session_key, { (op_type) DEL, { .r5 = { 1 } } } } } }
-	};
-
-	result_6 = del_1(&del_1_arg, clnt);
-	if (result_6 == (rpc_msg_ *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-
-	if (result_6->err)
-		printf("failure_message: %s, failure_code: %d\n\n",
-			result_6->rpc_msg__u.failure.failure_message,
-			result_6->rpc_msg__u.failure.failure_code);
-	else
-		printf("success_message: %s\n\n", 
-			result_6->rpc_msg__u.success.rpc_success_u.repl.success_message);
-
-	///////////////////////////////////////////////////////////////////////////
+void
+handle_store(CLIENT *clnt)
+{
+	printf("Executing store...\n");
 
 	rpc_msg_  *result_4;
 	rpc_msg_  store_1_arg = {
@@ -430,7 +181,7 @@ api_prog_1(char *host, char *input_file)
 			.rpc_success_u.repl.body
 			.rpc_reply_body_u.r3;
 		
-		string db_file = "client_db_" + string("client_1") + ".rpcdb";
+		string db_file = "client_db_" + string(username) + ".rpcdb";
 		ofstream out(db_file, ios::out | ios::binary);
 
 		if (!out) {
@@ -462,44 +213,100 @@ api_prog_1(char *host, char *input_file)
 				printf("%f%s", sd_vals[j], j == sd_len - 1 ? ")\n" : ", ");
 			}
 		}
+
 		printf("\n");
 		out.close();
-
-		ifstream in(db_file, ios::in | ios::binary);
-
-		unsigned int num_sensors;
-		unsigned int num_values;
-		float *values;
-
-		in.read((char *) &num_sensors, sizeof num_sensors);
-		sdl = (sensor_data *) calloc(num_sensors, sizeof(sensor_data));
-
-		printf("----> Number of sensors: %d\n", num_sensors);
-
-		for (int i = 0; i < num_sensors; i++) {
-			in.read((char *) &num_values, sizeof num_values);
-			in.read((char *) &sensor_id, sizeof sensor_id);
-
-			values = (float *) calloc(num_values, sizeof(float));
-			in.read((char *) &values, num_values * sizeof(float));
-
-			printf("\tSensor %d with %d values: (", sensor_id, num_values);
-
-			for (int j = 0; j < num_values; j++) {
-				printf("%f%s", values[j], j == num_values - 1 ? ")\n" : ", ");
-			}
-
-			sdl[i] = { sensor_id, { num_values, values } };
-		}
-		printf("\n");
-		in.close();
 	}
-	///////////////////////////////////////////////////////////////////////////
+}
+
+void
+handle_add(int sensor_id, int data_len, float *data, CLIENT *clnt)
+{
+	printf("Executing add %d %d [vals]...\n", sensor_id, data_len);
+
+	sensor_data sd = { sensor_id, { (unsigned int) data_len, data } };
+
+	rpc_msg_  *result_5;
+	rpc_msg_  add_1_arg = {
+		(err_type) SUCC,
+		{ { (msg_type_) REQ, { session_key, { (op_type) ADD, { .r4 = { sd } } } } } }
+	};
+
+	result_5 = add_1(&add_1_arg, clnt);
+	if (result_5 == (rpc_msg_ *) NULL) {
+		clnt_perror (clnt, "call failed");
+	}
+
+	if (result_5->err)
+		printf("failure_message: %s, failure_code: %d\n\n",
+			result_5->rpc_msg__u.failure.failure_message,
+			result_5->rpc_msg__u.failure.failure_code);
+	else
+		printf("success_message: %s\n\n", 
+			result_5->rpc_msg__u.success.rpc_success_u.repl.success_message);
+}
+
+void
+handle_del(int sensor_id, CLIENT *clnt)
+{
+	printf("Executing del %d...\n", sensor_id);
+
+	rpc_msg_  *result_6;
+	rpc_msg_  del_1_arg = {
+		(err_type) SUCC,
+		{ { (msg_type_) REQ, { session_key, { (op_type) DEL, { .r5 = { sensor_id } } } } } }
+	};
+
+	result_6 = del_1(&del_1_arg, clnt);
+	if (result_6 == (rpc_msg_ *) NULL) {
+		clnt_perror (clnt, "call failed");
+	}
+
+	if (result_6->err)
+		printf("failure_message: %s, failure_code: %d\n\n",
+			result_6->rpc_msg__u.failure.failure_message,
+			result_6->rpc_msg__u.failure.failure_code);
+	else
+		printf("success_message: %s\n\n", 
+			result_6->rpc_msg__u.success.rpc_success_u.repl.success_message);
+}
+
+void
+handle_update(int sensor_id, int data_len, float *data, CLIENT *clnt)
+{
+	printf("Executing update %d %d [vals]...\n", sensor_id, data_len);
+
+	sensor_data sd = { sensor_id, { (unsigned int) data_len, data } };
+
+	rpc_msg_ *result_7;
+	rpc_msg_  update_1_arg = {
+		(err_type) SUCC,
+		{ { (msg_type_) REQ, { session_key, { (op_type) UPDATE, { .r6 = { sd } } } } } }
+	};
+
+	result_7 = update_1(&update_1_arg, clnt);
+	if (result_7 == (rpc_msg_ *) NULL) {
+		clnt_perror (clnt, "call failed");
+	}
+
+	if (result_7->err)
+		printf("failure_message: %s, failure_code: %d\n\n",
+			result_7->rpc_msg__u.failure.failure_message,
+			result_7->rpc_msg__u.failure.failure_code);
+	else
+		printf("success_message: %s\n\n", 
+			result_7->rpc_msg__u.success.rpc_success_u.repl.success_message);
+}
+
+void
+handle_read(int sensor_id, CLIENT *clnt)
+{
+	printf("Executing read %d...\n", sensor_id);
 
 	rpc_msg_  *result_8;
 	rpc_msg_  read_1_arg = {
 		(err_type) SUCC,
-		{ { (msg_type_) REQ, { session_key, { (op_type) READ, { .r7 = { 2 } } } } } }
+		{ { (msg_type_) REQ, { session_key, { (op_type) READ, { .r7 = { sensor_id } } } } } }
 	};
 
 	result_8 = read_1(&read_1_arg, clnt);
@@ -529,13 +336,17 @@ api_prog_1(char *host, char *input_file)
 		}
 		printf("\n");
 	}
+}
 
-	///////////////////////////////////////////////////////////////////////////
-	stat_type stat = AVG;
+void
+handle_get_stat(int sensor_id, stat_type stat, CLIENT *clnt)
+{
+	printf("Executing get_stat %d...\n", sensor_id);
+
 	rpc_msg_  *result_9;
 	rpc_msg_  get_stat_1_arg = {
 		(err_type) SUCC,
-		{ { (msg_type_) REQ, { session_key, { (op_type) GET_STAT, { .r8 = { 1111, stat } } } } } }
+		{ { (msg_type_) REQ, { session_key, { (op_type) GET_STAT, { .r8 = { sensor_id, stat } } } } } }
 	};
 
 	result_9 = get_stat_1(&get_stat_1_arg, clnt);
@@ -556,10 +367,15 @@ api_prog_1(char *host, char *input_file)
 			stat == AVG ? "AVG" :
 			stat == MEDIAN ? "MEDIAN" :
 			"NONE",
-			result_9->rpc_msg__u.success.rpc_success_u.repl.body.rpc_reply_body_u.r8.stat, 2);
+			result_9->rpc_msg__u.success.rpc_success_u.repl.body.rpc_reply_body_u.r8.stat, sensor_id);
 	}
-	///////////////////////////////////////////////////////////////////////////
-	
+}
+
+void
+handle_get_stat_all(CLIENT *clnt)
+{
+	printf("Executing get_stat_all...\n");
+
 	rpc_msg_  *result_10;
 	rpc_msg_  get_stat_all_1_arg = {
 		(err_type) SUCC,
@@ -595,12 +411,121 @@ api_prog_1(char *host, char *input_file)
 				ssl[i].stats.stats_val[AVG],
 				ssl[i].stats.stats_val[MEDIAN]);
 		}
+
 		printf("\n");
 	}
-	///////////////////////////////////////////////////////////////////////////
+}
 
-	exit(0);
+void
+execute_input(string input_file, CLIENT *clnt)
+{
+	ifstream in(input_file);
+	string line, token;
 
+	if (!in) {
+		cout << "[FATAL]: Unable to open input file " << input_file << "!\n";
+		exit(1);
+	}
+
+	while (getline(in, line)) {
+		stringstream ss(line);
+		vector<string> tokens;
+
+		while (getline(ss, token, ' ')) {
+			tokens.push_back(token);
+		}
+
+		switch(op_stoi[tokens[TKN_CMD]]) {
+			case LOGIN: {
+				string username = tokens[TKN_USERNAME];
+				handle_login(username, clnt);
+				break;
+			}
+
+			case LOGOUT: {	
+				handle_logout(clnt);
+				break;
+			}
+			
+			case LOAD: {
+				handle_load(clnt);
+				break;
+			}
+
+			case STORE: {
+				handle_store(clnt);
+				break;
+			}
+
+			case ADD: {
+				int sensor_id = stoi(tokens[TKN_SID]);
+				int data_len = stoi(tokens[TKN_SDATA_LEN]);
+				float *data = (float *) calloc(data_len, sizeof(float));
+
+				for (int i = 0; i < data_len; i++) {
+					data[i] = stof(tokens[TKN_SDATA_VAL + i]);
+				}
+
+				handle_add(sensor_id, data_len, data, clnt);
+				break;
+			}
+
+			case DEL: {
+				int sensor_id = stoi(tokens[TKN_SID]);
+				handle_del(sensor_id, clnt);
+				break;
+			}
+
+			case UPDATE: {
+				int sensor_id = stoi(tokens[TKN_SID]);
+				int data_len = stoi(tokens[TKN_SDATA_LEN]);
+				float *data = (float *) calloc(data_len, sizeof(float));
+
+				for (int i = 0; i < data_len; i++) {
+					data[i] = stof(tokens[TKN_SDATA_VAL + i]);
+				}
+
+				handle_update(sensor_id, data_len, data, clnt);
+				break;
+			}
+
+			case READ: {
+				int sensor_id = stoi(tokens[TKN_SID]);
+				handle_read(sensor_id, clnt);
+				break;
+			}
+
+			case GET_STAT: {
+				int sensor_id = stoi(tokens[TKN_SID]);
+				stat_type stat = AVG;
+				handle_get_stat(sensor_id, stat, clnt);
+				break;
+			}
+
+			case GET_STAT_ALL: {
+				handle_get_stat_all(clnt);
+				break;
+			}
+			default: 
+				cout << "[ERROR]: Cannot execute command " << tokens[TKN_CMD] << "!\n";
+    	}
+	}
+
+	in.close();
+}
+
+void
+api_prog_1(char *host, char *input_file)
+{
+	CLIENT *clnt;
+
+	clnt = clnt_create (host, api_prog, api_vers, "udp");
+	if (clnt == NULL) {
+		clnt_pcreateerror (host);
+		exit (1);
+	}
+
+	execute_input(string(input_file), clnt);
 	clnt_destroy (clnt);
 }
 
